26d7be5dbd92fd7bab430a909866266f00e9f29f
Ivan04Aya <amarqueznt@gmail.com>
2022-12-06 18:48:32 -0600

    Agregar archivos de proyecto.

---
+++ TutoriasWcf/ITutoriasService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using TutoriasWcf.App_Data;
+
+namespace TutoriasWcf
+{
+    
+    [ServiceContract]
+    public interface ITutoriasService
+    {
+
+        [OperationContract]
+        Boolean registrarEstudiante(Estudiante estudiante);
+    }
+}
